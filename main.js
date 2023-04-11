8de2ac8e8a61da14a2b4326645502a50837f257d
minhhieu310 <nguyenphuocminhhieu310@gmail.com>
2023-04-11 21:02:26 +0700

    add main.js second time

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-// alert('Hello Javascript')
+var fullName = 'Nguyen Phuoc Minh Hieu'
+alert(fullName) 
+console.log(fullName)
+confirm(fullName)
+prompt(fullName)
+setTimeout(function(){
+    alert(fullName)
+}, 100)
+
+setTimeout(function(name) {
+    alert("Xin chào, " + name + "!");
+  }, 3000, "John");
+
+  setInterval(function(){
+    confirm(fullName)
+  }, 8000)
